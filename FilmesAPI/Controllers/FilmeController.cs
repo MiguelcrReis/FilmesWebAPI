2ab538092ba37bab5239731827d6f6932ec41cec
MiguelCrReis <miguelcr.reis@gmail.com>
2023-04-11 20:06:15 -0300

    Modificado a FilmeController Adicionado as Requisições Get: ConsultaFilmePorId e ConsultarFilmes

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -8,11 +8,25 @@ namespace FilmesAPI.Controllers;
 public class FilmeController : ControllerBase
 {
     private static List<Filme> filmes = new List<Filme>();
+    private static int id = 0;
 
     [HttpPost]
     public void AdicionaFilme([FromBody] Filme filme)
     {
+        filme.Id = id++;
         filmes.Add(filme);
-        Console.WriteLine($"{filme.Titulo}\n{filme.Genero}\n{filme.Duracao}");
+        Console.WriteLine($"{filme.Titulo}\n{filme.Genero}\n{filme.Diretor}\n{filme.Duracao}");
+    }
+
+    [HttpGet]
+    public IEnumerable<Filme> ConsultarFilmes()
+    {
+        return filmes;
+    }
+
+    [HttpGet("{id}")]
+    public Filme? ConsultaFilmePorId(int id)
+    {
+        return filmes.FirstOrDefault(f => f.Id == id);
     }
 }
