75cebd4bf775ba9f75106f75e55e0a9d33beb787
MiguelCrReis <miguelcr.reis@gmail.com>
2023-04-10 22:14:58 -0300

    Adicionado DataAnnotations na model Filme

--- FilmesAPI/Models/Filme.cs
+++ FilmesAPI/Models/Filme.cs
@@ -1,8 +1,20 @@
-﻿namespace FilmesAPI.Models;
+﻿
+
+using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Models;
 
 public class Filme
 {
+    [Required(ErrorMessage = "O {0} do filme é obrigatório!")]
+    [StringLength(60, MinimumLength = 3, ErrorMessage = "O tamanho do {0} deve ter entre {2} e {1} caractres")]
     public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O {0} do filme é obrigatório!")]
+    [StringLength(60, MinimumLength = 3, ErrorMessage = "O tamanho do {0} deve ter entre {2} e {1} caracteres")]
     public string Genero { get; set; }
+
+    [Required(ErrorMessage = "O {0} do filme é obrigatório!")]
+    [Range(70, 600, ErrorMessage = "A {0} do filme deve ter entre {1} e {2} minutos")]
     public int Duracao { get; set; }
 }
