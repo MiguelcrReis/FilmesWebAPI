ff45c419b165e82569baf0c2f190dc7d4b51fea9
MiguelCrReis <miguelcr.reis@gmail.com>
2023-04-11 22:29:03 -0300

    Utilizando DTOs para escrita de dados Adicionado as bibliotecas AutoMapper e AutoMapper.DependencyInjection Criado a classe CreateFilmeDto Criado a classe FilmeProfile Modificado a FilmeController para revisar os parâmetros através da FilmeDto

---
+++ FilmesAPI/Data/Dtos/CreateFilmeDto.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FilmesAPI.Data.Dtos;
+
+public class CreateFilmeDto
+{
+    [Required(ErrorMessage = "O {0} do filme é obrigatório!")]
+    [StringLength(60, MinimumLength = 3, ErrorMessage = "O tamanho do {0} deve ter entre {2} e {1} caractres")]
+    public string Titulo { get; set; }
+
+    [Required(ErrorMessage = "O {0} do filme é obrigatório!")]
+    [StringLength(60, MinimumLength = 3, ErrorMessage = "O tamanho do {0} deve ter entre {2} e {1} caracteres")]
+    public string Genero { get; set; }
+
+    [Required(ErrorMessage = "O {0} do filme é obrigatório!")]
+    public string Diretor { get; set; }
+
+    [Required(ErrorMessage = "O {0} do filme é obrigatório!")]
+    [Range(70, 600, ErrorMessage = "A {0} do filme deve ter entre {1} e {2} minutos")]
+    public int Duracao { get; set; }
+}
