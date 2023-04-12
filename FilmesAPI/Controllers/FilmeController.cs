aaf5256c377d113d189ad256487fe04404f33a5e
Miguel Reis <miguel.reis@megalaudo.com.br>
2023-04-12 11:35:31 -0300

    Adicionado PATCH e DELETE na controller Filme Criado a função de AtualizaFilmeParcial utilizando o verbo PATCH; Criado a função DeletaFilme utilizando o verbo DELETE.

--- FilmesAPI/Controllers/FilmeController.cs
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -2,6 +2,7 @@
 using FilmesAPI.Data;
 using FilmesAPI.Data.Dtos;
 using FilmesAPI.Models;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FilmesAPI.Controllers;
@@ -51,6 +52,38 @@ public class FilmeController : ControllerBase
         if (filme == null) return NotFound();
 
         _mapper.Map(filmeDto, filme);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public IActionResult AtualizaFilmeParcial(int id, JsonPatchDocument<UpdateFilmeDto> patch)
+    {
+        var filme = _context.Filmes.FirstOrDefault(f => f.Id == id);
+
+        if (filme == null) return NotFound();
+
+        var filmeAtualizar = _mapper.Map<UpdateFilmeDto>(filme);
+
+        patch.ApplyTo(filmeAtualizar, ModelState);
+
+        if (!TryValidateModel(filmeAtualizar)) return ValidationProblem(ModelState);
+
+        _mapper.Map(filmeAtualizar, filme);
+        _context.SaveChanges();
+
+        return NoContent();
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult DeletaFilme(int id)
+    {
+        var filme = _context.Filmes.FirstOrDefault(f => f.Id == id);
+
+        if (filme == null) return NotFound();
+
+        _context.Filmes.Remove(filme);
         _context.SaveChanges();
 
         return NoContent();
